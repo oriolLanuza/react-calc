d0a37d1c77ab6f85622e85b24f9f493ebf47072b
oriolLanuza <uri22lf@gmail.com>
2020-04-03 11:23:21 +0200

    working with ints

--- src/App.js
+++ src/App.js
@@ -7,58 +7,86 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: "",
-      currentArray: [],
-      val1: null,
-      val2: 0,
-      operation: ""
+      arrayNum: [],
+      val1: "",
+      val2: "",
+      operation: "",
+      display: ""
     };
     this.pressNumber = this.pressNumber.bind(this);
     this.pressOperation = this.pressOperation.bind(this);
-    this.pressCalculate = this.pressCalculate.bind(this);
+    this.pressEqual = this.pressEqual.bind(this);
+  }
+
+  pressNumber(num) {
+    this.state.arrayNum += num;
+    this.updateDisplay(num);
   }
 
   pressOperation(op) {
-    if (!this.state.operation) {
-      this.setState({ operation: op });
-      this.setState({ val1: +this.state.currentArray });
-      this.setState({ currentArray: [] });
-      this.addDisplay(op);
+    if (this.state.val1 == "") {
+      this.state.val1 = +this.state.arrayNum;
+    }
+    if (this.state.operation == "") {
+      this.state.operation = op;
+      this.updateDisplay(op);
+      console.log(this.state);
+    } else if (this.state.val2 == "") {
+      this.state.val2 = +this.state.arrayNum;
+      this.state.val1 = this.calculate();
+      this.state.operation = op;
+      this.state.display = "" + this.state.val1 + this.state.operation;
+      this.updateDisplay("");
+    }
+    this.state.arrayNum = [];
+    this.state.val2 = "";
+  }
+
+  pressEqual() {
+    if (!(this.state.operation == "")) {
+      this.state.val2 = +this.state.arrayNum;
+      this.state.val1 = this.calculate();
+      this.state.display = "";
+      this.updateDisplay(this.state.val1);
+      this.state.arrayNum = [];
+      this.state.val2 = "";
     }
   }
 
-  pressNumber(x) {
-    this.setState({ currentArray: (this.state.currentArray += x) });
-    this.addDisplay(x);
-    this.setState({ val2: +this.state.currentArray });
-  }
-
-  addDisplay(txt) {
-    this.setState({ display: (this.state.display += txt) });
-  }
-
-  pressClear() {
-    this.setState({
-      display: "",
-      val1: 0,
-      val2: 0,
-      operation: "",
-      currentArray: []
-    });
-  }
-
-  pressCalculate() {
-    console.log(this.state);
+  calculate() {
     switch (this.state.operation) {
       case "+":
-        this.setState({ display: `${this.state.val1+this.state.val2}` });
-        this.setState({ val1: +this.state.currentArray });
-
+        return this.state.val1 + this.state.val2;
+        break;
+        case "-":
+        return this.state.val1 - this.state.val2;
+        break;
+        case "*":
+        return this.state.val1 * this.state.val2;
+        break;
+        case "/":
+        return this.state.val1 / this.state.val2;
         break;
 
       default:
         break;
     }
+  }
+
+  pressClear() {
+    this.setState({
+      arrayNum: [],
+      val1: "",
+      val2: "",
+      operation: "",
+      display: ""
+    });
+  }
+
+  updateDisplay(newTxt) {
+    let oldTxt = this.state.display;
+    let txt = oldTxt + newTxt;
+    this.setState({ display: txt });
   }
 
   render() {
@@ -72,13 +100,23 @@ export default class App extends React.Component {
                 onClick={() => this.pressNumber(7)}
                 outline
                 color="primary"
+                className="btn-calc"
               >
                 7
               </Button>{" "}
-              <Button outline color="primary">
+              <Button
+                onClick={() => this.pressNumber(8)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 8
-              </Button>{" "}
-              <Button outline color="primary">
+              </Button>{" "}<Button
+                onClick={() => this.pressNumber(9)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 9
               </Button>{" "}
               <Button
@@ -89,13 +127,26 @@ export default class App extends React.Component {
                 /
               </Button>{" "}
               <br />
-              <Button outline color="primary">
+              <Button
+                onClick={() => this.pressNumber(4)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 4
-              </Button>{" "}
-              <Button outline color="primary">
+              </Button>{" "}<Button
+                onClick={() => this.pressNumber(5)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 5
-              </Button>{" "}
-              <Button outline color="primary">
+              </Button>{" "}<Button
+                onClick={() => this.pressNumber(6)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 6
               </Button>{" "}
               <Button
@@ -106,13 +157,26 @@ export default class App extends React.Component {
                 *
               </Button>{" "}
               <br />
-              <Button outline color="primary">
+              <Button
+                onClick={() => this.pressNumber(1)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 1
-              </Button>{" "}
-              <Button outline color="primary">
+              </Button>{" "}<Button
+                onClick={() => this.pressNumber(2)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 2
-              </Button>{" "}
-              <Button outline color="primary">
+              </Button>{" "}<Button
+                onClick={() => this.pressNumber(3)}
+                outline
+                color="primary"
+                className="btn-calc"
+              >
                 3
               </Button>{" "}
               <Button
@@ -129,11 +193,7 @@ export default class App extends React.Component {
               <Button outline color="primary">
                 0
               </Button>{" "}
-              <Button
-                onClick={() => this.pressCalculate()}
-                outline
-                color="primary"
-              >
+              <Button onClick={() => this.pressEqual()} outline color="primary">
                 =
               </Button>{" "}
               <Button
